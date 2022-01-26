5e5dfe4301a0779bf99d3f475102bf66ce70d361
Simone Ruffilli <sruffilli@google.com>
2022-01-26 20:47:39 +0100

    Variables

--- modules/hetzner-lab/outputs.tf
+++ modules/hetzner-lab/outputs.tf
@@ -1,6 +1,6 @@
 output "ips" {
   value = {
-    vpngw-public  = hcloud_server.vpngw.ipv4_address
-    vpngw-private = hcloud_server_network.vpngw_network.ip
+    public  = hcloud_server.vpngw.ipv4_address
+    private = hcloud_server_network.vpngw_network.ip
   }
 }
